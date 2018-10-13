44519b3c4326194f3d78c2a9303b542ec49c438f
Chris Cornutt <enygma@phpdeveloper.org>
2018-10-12 19:48:34 -0500

    adding step 2 tests

---
+++ step2.php
@@ -0,0 +1,5 @@
+<?php
+
+$array = [
+    'test1', 'test2'
+];
---
+++ tests/step2Test.php
@@ -0,0 +1,27 @@
+<?php
+
+// Include our index page
+require_once __DIR__.'/../index.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class Step2Test extends TestCase
+{
+    public function testChangedValue() : void
+    {
+        global $array;
+        
+        $value = strtolower($array[1]);
+        $this->assertEquals($value, 'test4');
+    }
+
+    public function testAddNewValue() : void
+    {
+        global $array;
+
+        // Pop off the last value and check it
+        $value = strtolower(array_pop($array));
+        $this->assertEquals('pineapple', $value);
+    }
+}
+
